a58918bb4f8903105f383568548a2341c4fc5d5a
Asadbek Raimov <raimovasadbek18@gmail.com>
2021-12-05 23:04:04 +0500

    feat: add beautiful logging

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-const fastify = require('fastify')({})
-// const swaggerUI = require('swagger-ui-express');
-// const path = require('path');
-// const YAML = require('yamljs');
-
-// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
-
-
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+const fastify = require('fastify')({
+  logger:{ 
+    prettyPrint:true
+  }
+})
 
 
-
+fastify.register(require('fastify-swagger'),{
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger:{
+    info: {title: 'fastify-api'},
+  }
+})
 fastify.register(require('./routers/user.router'))
 fastify.register(require('./routers/boards.router'))
 fastify.register(require('./routers/task.router'))
@@ -21,3 +23,13 @@ fastify.get('/',(req,res)=>{
 
 
 module.exports = fastify;
+
+
+// const swaggerUI = require('swagger-ui-express');
+// const path = require('path');
+// const YAML = require('yamljs');
+
+// const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+
+
+// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
